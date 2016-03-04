fb600a48944ffcbc88a086d75736580e744e106d
epassos26 <epassos26@gmail.com>
2016-03-04 17:24:01 +0000

    Added sleep methods + member in Dragon

--- src/cli/Dragon.java
+++ src/cli/Dragon.java
@@ -2,15 +2,15 @@ package cli;
 
 public class Dragon extends Actor {
 
-	boolean dead;
-	
+	boolean dead, asleep;
+
 	//Constructor
 	public Dragon(int x, int y){
 		super(x,y);
 		this.c = 'D';
 		this.dead = false;
 	}
-	
+
 	//Checks if Dragon is dead
 	public boolean isDead(){
 		return dead;
@@ -18,6 +18,18 @@ public class Dragon extends Actor {
 	//Kills Dragon
 	public void killDragon(){
 		dead = true;
+	}
+
+	public boolean isAsleep(){
+		return asleep;
+	}
+
+	public void awake(){
+		asleep = false;
+	}
+
+	public void fallAsleep(){
+		asleep = true;
 	}
 
 }
