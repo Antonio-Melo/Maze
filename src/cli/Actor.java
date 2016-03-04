4dcdf423d14fec788e5bc44f15ecbbd75919c831
epassos26 <epassos26@gmail.com>
2016-03-04 17:23:37 +0000

    Added Actor class

---
+++ src/cli/Actor.java
@@ -0,0 +1,21 @@
+package cli;
+
+public class Actor {
+	int x,y;
+	char c;
+	
+	public Actor(int x, int y){
+		this.x = x;
+		this.y = y;
+	}	
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	
+}
